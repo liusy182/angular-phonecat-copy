27cf5880901c789149f6c1276f2d83968f243971
liusy182 <liusy182@hotmail.com>
2015-08-18 23:18:50 +0800

    step 5

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,21 +4,10 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function ($scope) {
-  $scope.name = "World";
-
-  $scope.phones = [
-    {'name': 'Nexus S',
-     'snippet': 'Fast just got faster with Nexus S.',
-     'age': 2},
-    {'name': 'Motorola XOOM™ with Wi-Fi',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 1},
-    {'name': 'MOTOROLA XOOM™',
-     'snippet': 'The Next, Next Generation tablet.',
-     'age': 3}
-  ];
-  
-  $scope.orderProp = 'age'; //initial value
-
-});
+phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', 
+  function ($scope, $http) {
+    $http.get('phones/phones.json').then(function(response){
+      $scope.phones = response.data;
+    });
+    $scope.orderProp = 'age';
+}]);
