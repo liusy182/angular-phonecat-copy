c304d0b4997ea232a35d333b7ac928c1ee2a42e8
liusy182 <liusy182@hotmail.com>
2015-08-20 22:20:19 +0800

    step 8

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -13,7 +13,9 @@ phonecatControllers.controller('PhoneListCtrl', ['$scope', '$http',
   }]);
 
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope','$routeParams', '$http',
+  function ($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response){
+      $scope.phone = response.data;
+    });
   }]);
